49c661f9ea2b287c078f0ba59cf571b9b24eb183
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2023-07-11 18:25:14 -0500

    Fixup worldgen issues

--- src/main/java/dev/sterner/geocluster/common/registry/GeoclusterWorldgenRegistry.java
+++ src/main/java/dev/sterner/geocluster/common/registry/GeoclusterWorldgenRegistry.java
@@ -1,63 +1,72 @@
-package dev.sterner.geocluster.common.registry;
+package dev.sterner.geocluster.common.world.feature;
 
-import com.google.common.collect.Lists;
+import com.mojang.serialization.Codec;
 import dev.sterner.geocluster.Geocluster;
-import dev.sterner.geocluster.common.world.feature.DepositFeature;
-import dev.sterner.geocluster.mixin.BiomeModificationContextImplMixin;
-import net.fabricmc.fabric.api.biome.v1.*;
-import net.fabricmc.fabric.api.event.registry.DynamicRegistryView;
-import net.minecraft.registry.*;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.tag.TagKey;
-import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import dev.sterner.geocluster.GeoclusterConfig;
+import dev.sterner.geocluster.api.DepositCache;
+import dev.sterner.geocluster.api.IDeposit;
+import dev.sterner.geocluster.common.components.*;
+import dev.sterner.geocluster.common.utils.FeatureUtils;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.gen.chunk.FlatChunkGenerator;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.util.FeatureContext;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class GeoclusterWorldgenRegistry {
-
-    public static Feature<DefaultFeatureConfig> DEPOSIT_FEATURE = Registry.register(Registries.FEATURE, "deposits", new DepositFeature(DefaultFeatureConfig.CODEC));
-    public static ConfiguredFeature<DefaultFeatureConfig, Feature<DefaultFeatureConfig>> CONFIGURED_DEPOSIT_FEATURE;
-    public static RegistryKey<ConfiguredFeature<?, ?>> CONFIGURED_DEPOSIT_FEATURE_KEY;
-
-    public static PlacedFeature PLACED_DEPOSIT_FEATURE;
-    public static RegistryKey<PlacedFeature> PLACED_DEPOSIT_FEATURE_KEY;
-
-    public static void init() {
-
+public class DepositFeature extends Feature<DefaultFeatureConfig> {
+    public DepositFeature(Codec<DefaultFeatureConfig> configCodec) {
+        super(configCodec);
     }
 
-    public static void init(DynamicRegistryView registryView, Registry<ConfiguredFeature<?, ?>> configuredFeatures) {
+    @Override
+    public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
+        if (context.getGenerator() instanceof FlatChunkGenerator) {
+            return false;
+        }
 
-        CONFIGURED_DEPOSIT_FEATURE = Registry.register(configuredFeatures, "deposits_configured", new ConfiguredFeature<>(DEPOSIT_FEATURE, DefaultFeatureConfig.INSTANCE));
-        CONFIGURED_DEPOSIT_FEATURE_KEY = ConfiguredFeatures.of("deposits_configured");
+        StructureWorldAccess world = context.getWorld();
+        BlockPos pos = context.getOrigin();
 
-        registryView.getOptional(RegistryKeys.PLACED_FEATURE).ifPresent(registry -> {
-            RegistryEntryLookup<ConfiguredFeature<?, ?>> entry = registryView.asDynamicRegistryManager().createRegistryLookup().getOrThrow(RegistryKeys.CONFIGURED_FEATURE);
-            PLACED_DEPOSIT_FEATURE = new PlacedFeature(entry.getOrThrow(CONFIGURED_DEPOSIT_FEATURE_KEY), Lists.newArrayList(HeightRangePlacementModifier.uniform(YOffset.fixed(-64), YOffset.fixed(320))));
-            Registry.register(registry, "deposits_placed", PLACED_DEPOSIT_FEATURE);
-        });
-        PLACED_DEPOSIT_FEATURE_KEY = PlacedFeatures.of("deposits_placed");
 
-        BiomeModification modifications = BiomeModifications.create(Geocluster.id("worldgen"));
-        modifications.add(ModificationPhase.ADDITIONS, BiomeSelectors.all(), ctx -> {
-            ctx.getGenerationSettings().addFeature(GenerationStep.Feature.UNDERGROUND_ORES, PLACED_DEPOSIT_FEATURE_KEY);
-        });
+        WorldDepositComponent depositComponent = GeoclusterComponents.DEPOSIT.get(world.toServerWorld());
+        WorldChunkComponent chunkComponent = GeoclusterComponents.CHUNK.get(world.toServerWorld());
 
-        modifications.add(ModificationPhase.REMOVALS, BiomeSelectors.all(), ctx -> {
-            Iterable<RegistryEntry<PlacedFeature>> registryEntries = getPlacedFeaturesByTag(ctx, GeoclusterTagRegistry.ORES_TO_REMOVE);
-            for (RegistryEntry<PlacedFeature> placedFeatureHolder : registryEntries) {
-                if (placedFeatureHolder.getKey().isPresent()) {
-                    ctx.getGenerationSettings().removeFeature(placedFeatureHolder.getKey().get());
+        boolean placedCluster = false;
+        boolean placedPending = placePendingBlocks(world, depositComponent, chunkComponent, pos);
+
+        if (world.getRandom().nextDouble() > GeoclusterConfig.CHUNK_SKIP_CHANCE) {
+            for (int p = 0; p < GeoclusterConfig.NUMBER_CLUSTERS_PER_CHUNK; p++) {
+                IDeposit deposit = DepositCache.getCache().pick(world, pos);
+                if (deposit == null) {
+                    continue;
+                }
+
+                boolean anyGenerated = deposit.generate(world, pos, depositComponent, chunkComponent) > 0;
+                if (anyGenerated) {
+                    placedCluster = true;
+                    deposit.generatePost(world, pos, depositComponent, chunkComponent);
                 }
             }
-        });
+        }
+
+        chunkComponent.setChunkGenerated(new ChunkPos(pos));
+
+
+        return placedCluster || placedPending;
     }
 
-    private static Iterable<RegistryEntry<PlacedFeature>> getPlacedFeaturesByTag(BiomeModificationContext ctx, TagKey<PlacedFeature> placedFeatureTagKey) {
-        DynamicRegistryManager dynamicRegistryManager = ((BiomeModificationContextImplMixin) ctx).getRegistries();
-        Registry<PlacedFeature> placedFeatureRegistry = dynamicRegistryManager.get(RegistryKeys.PLACED_FEATURE);
-        return placedFeatureRegistry.iterateEntries(placedFeatureTagKey);
+    private boolean placePendingBlocks(StructureWorldAccess worldAccess, IWorldDepositComponent depCap, IWorldChunkComponent cgCap, BlockPos origin) {
+        ChunkPos cp = new ChunkPos(origin);
+        ConcurrentLinkedQueue<WorldDepositComponent.PendingBlock> q = depCap.getPendingBlocks(cp);
+        if (cgCap.hasChunkGenerated(cp) && q.size() > 0) {
+            Geocluster.LOGGER.info("Chunk [{}, {}] has already generated but we're trying to place pending blocks anyways", cp.x, cp.z);
+        }
+        q.forEach(x -> FeatureUtils.enqueueBlockPlacement(worldAccess, x.pos(), x.state(), depCap, cgCap));
+        depCap.removePendingBlocksForChunk(cp);
+        return q.size() > 0;
     }
 }
