770874092034f5d3f9a9621588682ecd13641a10
mrsterner <simon@sterner.dev>
2023-08-11 17:20:54 +0200

    fix #7

--- src/main/java/dev/sterner/geocluster/datagen/GeoclusterLootTableProvider.java
+++ src/main/java/dev/sterner/geocluster/datagen/GeoclusterLootTableProvider.java
@@ -44,6 +44,8 @@ public class GeoclusterLootTableProvider extends FabricBlockLootTableProvider {
         this.addDrop(QUARTZ_ORE, b -> oreDrops(b, QUARTZ));
         this.addDrop(DEEPSLATE_QUARTZ_ORE, b -> oreDrops(b, QUARTZ));
 
+        this.addDrop(ANCIENT_DEBRIS_ORE, RAW_ANCIENT_DEBRIS);
+
         this.addDrop(COPPER_SAMPLE, Items.RAW_COPPER);
         this.addDrop(IRON_SAMPLE, Items.RAW_IRON);
         this.addDrop(GOLD_SAMPLE, Items.RAW_GOLD);
